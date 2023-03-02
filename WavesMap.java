70db575b6d0cb205197bce574ee3a029e5fd0bac
GromDv <gromdv@gmail.com>
2023-03-02 19:04:55 +0300

    Add wawe realisation

--- WavesMap.java
+++ WavesMap.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class WavesMap {
 
     int[][] map;
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BKYELLOW = "\u001B[43m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BKRED = "\u001B[41m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_BKBLUE = "\u001B[44m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BKGREEN = "\u001B[42m";
 
     public void CreateMap() {
         int[][] map = {
@@ -24,34 +35,83 @@ public class WavesMap {
         this.map = map;
     }
 
-    public void SetStartPoint(int x, int y) {
-        map[y][x] = -2;
+    public boolean isEmpty(Point2d a) {
+        return this.map[a.getY()][a.getX()] == 0;
     }
 
-    public void SetEndtPoint(int x, int y) {
-        map[y][x] = -3;
+    public boolean isBorder(Point2d a) {
+        return this.map[a.getY()][a.getX()] == -1;
     }
 
-    public void PrintMap() {
+    public void SetStartPoint(Point2d a) {
+        this.SetPoint(a, -2);
+    }
+
+    public boolean isStart(Point2d a) {
+        return this.map[a.getY()][a.getX()] == -2;
+    }
+
+    public void SetEndPoint(Point2d a) {
+        this.SetPoint(a, -3);
+    }
+
+    public boolean isEnd(Point2d a) {
+        return this.map[a.getY()][a.getX()] == -3;
+    }
+
+    public void SetPoint(Point2d a, int value) {
+        map[a.getY()][a.getX()] = value;
+    }
+
+    public int GetPoint(Point2d a) {
+        return this.map[a.getY()][a.getX()];
+    }
+
+    /**
+     * Печать карты с маршрутом
+     * @param road - список точек маршрута
+     */
+    public void PrintMap(ArrayList<Point2d> road) {
+        Point2d pt = new Point2d(0,0);
+        int x = 0, y = 0;
         for (int[] is : map) {
+            x = 0;
             for (int i : is) {
                 switch(i){
                     case -1:
-                        System.out.printf("%s", "⛌");
+                        System.out.printf(ANSI_BKBLUE + "%s" + ANSI_RESET, "  ");
                         break;
                     case 0:
-                        System.out.printf("%s", " ");
+                        System.out.printf("%s", "  ");
                         break;
                     case -2:
-                        System.out.printf("%s", "S");
+                        System.out.printf(ANSI_BKGREEN + "%s" + ANSI_RESET, "SS");
                         break;
                     case -3:
-                        System.out.printf("%s", "E");
+                        System.out.printf(ANSI_BKRED + "%s" + ANSI_RESET, "EE");
+                        break;
+                    case -4:
+                        System.out.printf(ANSI_BKRED + "%s" + ANSI_RESET, "OO");
+                        break;
+                    default:
+                        pt.setX(x);
+                        pt.setY(y);
+                        int flag = 0;
+                        for (Point2d point : road) {
+                            if(point.equals(pt)) {
+                                System.out.printf(ANSI_BKYELLOW + "%2d" + ANSI_RESET, i);
+                                flag = 1;
+                            }
+                        }
+                        if(flag == 0) 
+                            System.out.printf("%2d", i);
                         break;
                 }
+                x ++;
             }
             System.out.println();
+            y ++;
         }
     }
-
 }
+
--- appWaves.java
+++ appWaves.java
@@ -1,13 +1,90 @@
+import java.util.ArrayList;
+
 public class appWaves {
 
+    static WavesMap wMap = new WavesMap();
+    static ArrayList<Point2d> route = new ArrayList<Point2d>();
+    static int ccc = 0;
+
+    /**
+     * Реализация волнового алгоритма построения маршрута
+     * @param args
+     */
     public static void main(String[] args) {
-        
-        WavesMap wMap = new WavesMap();
 
         wMap.CreateMap();
-        wMap.SetStartPoint(2, 11);
-        wMap.SetEndtPoint(12, 1);
-        wMap.PrintMap();
+        Point2d StartPt = new Point2d(2, 11);
+        wMap.SetStartPoint(StartPt);
+        Point2d EndPt = new Point2d(12, 4);
+        wMap.SetEndPoint(EndPt);
+
+        wMap.PrintMap(route);
+
+        WaveRouting(StartPt, 0);
+
+        FindRoteEndPoint(EndPt);
+
+        System.out.println();
+        wMap.PrintMap(route);
+     }
+
+    /**
+     * Волновой расчет шагов по карте (рекурсия)
+     * @param a - стартовая точка
+     * @param step - текущий шаг
+     */
+    public static void WaveRouting(Point2d a, int step) {
+        if (wMap.isEmpty(a) || wMap.isStart(a) || wMap.GetPoint(a) > step) {
+            if (!wMap.isStart(a)) {
+                wMap.SetPoint(a, step);
+            }
+            WaveRouting(a.incX(), step + 1);
+            WaveRouting(a.decY(), step + 1);
+            WaveRouting(a.decX(), step + 1);
+            WaveRouting(a.incY(), step + 1);
+        }
+    }
+    
+    /**
+     * Рекурсивное построение списка маршрутных точек начиная с конца
+     * @param a - конечная точка маршрута
+     */
+    public static void FindRoteEndPoint(Point2d a) {
+        route.add(a);
+
+        if (wMap.isStart(a))
+            return;
+
+        Point2d res = new Point2d(a);
+
+        int tmp = 100;
+        Point2d tp1 = a.incX();
+        if (!wMap.isBorder(tp1) && !wMap.isEnd(tp1)) {
+            tmp = wMap.GetPoint(tp1);
+            res = tp1.copy();
+        }
+
+        Point2d tp2 = a.incY();
+        if (!wMap.isBorder(tp2) && !wMap.isEnd(tp2))
+            if (tmp > wMap.GetPoint(tp2)) {
+                tmp = wMap.GetPoint(tp2);
+                res = tp2.copy();
+            }
+
+        Point2d tp3 = a.decX();
+        if (!wMap.isBorder(tp3) && !wMap.isEnd(tp3))
+            if (tmp > wMap.GetPoint(tp3)) {
+                tmp = wMap.GetPoint(tp3);
+                res = tp3.copy();
+            }
+
+        Point2d tp4 = a.decY();
+        if (!wMap.isBorder(tp4) && !wMap.isEnd(tp4))
+            if (tmp > wMap.GetPoint(tp4)) {
+                tmp = wMap.GetPoint(tp4);
+                res = tp4.copy();
+            }
+
+        FindRoteEndPoint(res);
     }
 }
-
